a604f3447569de46d2d750dfae23cce83e5ca45c
Chuta Sano <chuta_japan@comcast.net>
2017-10-10 12:03:02 -0400

    auto pushed by gitp

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 THISPATH=$(dirname $(readlink -f $0))
 
 sudo apt install vim xclip ack-grep tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --silent
 
 ln -sf ${THISPATH}/vimrc ~/.vimrc
 ln -sf ${THISPATH}/ackrc ~/.ackrc
